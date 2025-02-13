913f2f0530d0b194a5839266a58135a65ef4e0dd
siwilmarr <wilmarardhana9@gmail.com>
2025-02-13 21:29:34 +0700

    Mengerjakan Tugas Pertemuan 2

---
+++ src/Pertemuan2/Node.java
@@ -0,0 +1,27 @@
+package Pertemuan2;
+
+public class Node {
+	private int nilai;
+	private Node next;
+	
+	//inisialisasi Node
+	public Node (int nilai) {
+		this.nilai = nilai;
+	}
+
+	public int getNilai() {
+		return nilai;
+	}
+
+	public void setNilai(int nilai) {
+		this.nilai = nilai;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
